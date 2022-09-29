5331d6e2e4aa8dd8338cce77d4abb7b69848fa88
Fadesola <iyaomolereibukun@gmail.com>
2022-09-29 12:52:04 +0100

    initial commit

---
+++ understanding print.py
@@ -0,0 +1,5 @@
+'''print('fadesola','ibukun','england','france', sep='+')
+print('fadesolaibukun', 'gmail.com', sep='@')
+print('', '20 ', '30 ', '40 ', '50 ', sep='$')'''
+print('hello', end='\n')
+print('world')
